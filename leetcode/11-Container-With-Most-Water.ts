4c67d37e9fbe5977461e15c4553d76dd70487dfb
mhShohan <mehdihasanshohan25@gmail.com>
2026-01-13 23:11:32 +0600

    leetcode - 11. Container With Most Water

---
+++ leetcode/11-Container-With-Most-Water.ts
@@ -0,0 +1,38 @@
+/**
+ * 11. Container With Most Water
+ * https://leetcode.com/problems/container-with-most-water/
+ * @ Steps to solve:
+ * 1. Initialize two pointers, one at the start (left) and one at the end (right) of the array.
+ * 2. Calculate the area formed between the two pointers using the formula: area = min(height[left], height[right]) * (right - left).
+ * 3. Update the maximum area found so far if the current area is larger.
+ * 4. Move the pointer pointing to the shorter line inward (either left++ or right--) to potentially find a taller line that could increase the area.
+ * 5. Repeat steps 2-4 until the two pointers meet.
+ * 6. Return the maximum area found.
+ */
+
+function maxArea(height: number[]): number {
+  let max = 0;
+
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+    const h = Math.min(height[left], height[right]);
+    const w = right - left;
+    const area = h * w;
+
+    if (max < area) {
+      max = area;
+    }
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return max;
+}
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
